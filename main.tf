73a8c99e19b4e4fff7adec7a70517bc9f7bd95e6
lkkaira <lalit.kaira@hcltech.com>
2025-11-24 16:50:14 +0530

    Update main.tf-2

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@
 module "storage" {
   source              = "./modules/storage"
   storage_name        = var.storage_name
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.resource_group_name
   location            = var.location
 }
